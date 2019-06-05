e93f1111722eb9854c9cb2ad62dc2bc8c4f66199
Bunkka <Bunkka_kzru@mail.ru>
2019-06-05 03:10:31 +0300

    Отрисовано облако для результатов

---
+++ js/stat.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_WIDTH = 20;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+};
